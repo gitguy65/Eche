4ec2d996c048ba95889cf381c2a7f11bb3a350d5
gitguy65 <newsailor958@outlook.com>
2025-07-13 02:05:45 +0100

    fixes

--- Eche/Program.cs
+++ Eche/Program.cs
@@ -1,85 +1,84 @@
 ﻿public class Program
 {
-	
-	public static void Main()
+    public static void Main()
 	{
 		Board.InitializeBoard();
 		Player PlayerOne = new Player(0);
 		Player PlayerTwo = new Player(1);
 		
 		Game.PlayerOne = PlayerOne;
-      Game.PlayerTwo = PlayerTwo;
+		Game.PlayerTwo = PlayerTwo;
 		
 		StartGame.Begin();
 	}
 }
 	
 public static class StartGame
-{
-	public static Player PlayerOne;
-	public static Player PlayerTwo;
-	
+{	
 	public static void Begin()
 	{
 	
 		Console.WriteLine(@"
+Éché
+An exciting board game
 		
-		Éché
-		An exciting board game
+4	4	4	4	4	4
+4	4	4	4	4	4
 		
-		{4}  {4}  {4}  {4}  {4}  {4}
-		{4}  {4}  {4}  {4}  {4}  {4}
+11	10	9	8	7	6
+0	1	2	3	4	5
 		
-		 11  10    9	 8    7    6
-		 0    1    2    3    4    5
+Pick a side of the board and capture the most seeds.
 		
-		Pick a side of the board and capture the most seeds.
+_How to play_
+Choose a side of the game board and pick any hole on your side with at least one seed to get started.
 		
-		How to:
-		Choose a side of the game board and pick any hole on your side with at least one seed to get started.
-		
-		Enter '1' to select the top row or '2' to select the bottom row
-		");
+Enter '1' to select the top row or '2' to select the bottom row
+");
 		
 		string playerInput = Console.ReadLine()!;
 		
 		if (playerInput == "1") 
 		{
 			Console.WriteLine("You are player one");
-			Console.WriteLine("Pick any hole from 6 - 11");
+			Console.WriteLine("Pick any hole from 0 - 5");
 			string startHoleInput = Console.ReadLine()!;
-			
+
 			if(int.TryParse(startHoleInput, out int startHole))
 			{
-				if(startHole >= 7 && startHole <= 11)
-					Game.PlayGame(PlayerOne, startHole);
-			}
+				if(startHole >= 0 && startHole < 6)
+				{
+                    Game.PlayGame(Game.PlayerOne, startHole);
+                }
+            }
 			else 
 			{
 				Console.WriteLine("Invalid move");
-			}
-			
+                Game.PlayGame(Game.PlayerOne, startHole);
+            }
 		}
 		
 		else if (playerInput == "2")
 		{
-			Console.WriteLine("You are player one");
+			Console.WriteLine("You are player two");
 			Console.WriteLine("Pick any hole from 6 - 11");
 			string startHoleInput = Console.ReadLine()!;
 			
 			if(int.TryParse(startHoleInput, out int startHole))
 			{
-				if(startHole >= 0 && startHole <= 6)
-					Game.PlayGame(PlayerTwo, startHole);
-			}
-			
-		}
+				if(startHole >= 6 && startHole < 12)
+				{
+                    Game.PlayGame(Game.PlayerTwo, startHole);
+                }
+            }
+        }
 
-		else
+        else
 		{
 			Console.WriteLine("Invalid player");
-		}
-	}
+            throw new Exception();
+        }
+    }
 }
 
 public static class Board
@@ -112,28 +111,32 @@ public static class Game
 	public static Player CurrentPlayer { get; set; }
 	public static Player PlayerOne { get; set; }
 	public static Player PlayerTwo { get; set; }
-	public static int NumberOfPickedSeeds { get; set; }
+	public static int NumberOfPickedSeeds { get; private set; }
 	
 	public static void PlayGame(Player currentPlayer, int hole)
 	{
+		CurrentPlayer = currentPlayer;
+
 		NumberOfPickedSeeds = Board.Holes[hole];
-		Board.Holes[hole] = 0;
-		
+
 		if(NumberOfPickedSeeds == 0)
 		{
 			Console.WriteLine("Invalid move. Hole is empty");
 			return;
 		}
-			
-		if(CurrentPlayer.Index == 0 && hole > 7) 
-		{	
-			Move(currentPlayer, NumberOfPickedSeeds, hole++);
-		} 
-		else if(CurrentPlayer.Index == 1 && hole < 7)
-		{	
+
+		Board.Holes[hole] = 0;
+		Console.WriteLine($"You picked {NumberOfPickedSeeds} seeds from hole {hole}");
+
+		if (currentPlayer.Index == 0 && hole >= 0 && hole < 6)
+		{
 			Move(currentPlayer, NumberOfPickedSeeds, hole++);
 		}
-		else 
+		else if (currentPlayer.Index == 1 && hole > 5 && hole <= 11)
+		{
+			Move(currentPlayer, NumberOfPickedSeeds, hole++);
+		}
+		else
 		{
 			Console.WriteLine("Invalid move. Select from your side");
 		}
@@ -143,79 +146,76 @@ public static class Game
 	
 	private static void Move(Player currentPlayer, int numberOfPickedSeeds, int hole)
 	{
-		while (numberOfPickedSeeds > 0) 
+		NumberOfPickedSeeds = numberOfPickedSeeds;
+		while (NumberOfPickedSeeds > 0) 
 		{
-			hole++; 
-			
-			if(numberOfPickedSeeds == 1 && Board.Holes[hole] == 3)
+			hole++;
+
+			if (NumberOfPickedSeeds == 1 && Board.Holes[hole] == 3)
 			{
-				numberOfPickedSeeds = 0;
+				NumberOfPickedSeeds = 0;
 				Board.Holes[hole] = 0;
 				currentPlayer.Score++;
+				break;
 			}
 			
-			if(numberOfPickedSeeds == 1 && Board.Holes[hole] > 0)
+			if(NumberOfPickedSeeds == 1 && Board.Holes[hole] > 0)
 			{
-				numberOfPickedSeeds = Board.Holes[hole]++;
+				NumberOfPickedSeeds = Board.Holes[hole] + 1;
 				Board.Holes[hole] = 0;
 			}
 		
 			if(hole == 11)
-				 hole = 0;
+				hole = 0;
 				
-			Board.Holes[hole] = Board.Holes[hole]++;
-			numberOfPickedSeeds--;
-			GameState.CheckGame(CurrentPlayer, PlayerOne, PlayerTwo);
-			Move(currentPlayer, numberOfPickedSeeds, hole);
+			Board.Holes[hole]++;
+			NumberOfPickedSeeds--;
+			GameState.CheckGame();
+			Move(currentPlayer, NumberOfPickedSeeds, hole);
 		}
 		
-		string newHoleInput;
-    int newHole;
+		int newHole;
 		
 		if (currentPlayer.Index == PlayerTwo.Index)
-    {
-      currentPlayer = PlayerTwo;
-      Console.WriteLine("PlayerTwo's turn, select a seeded hole from the top row");
-      newHoleInput = Console.ReadLine();
-      int.TryParse(newHoleInput, out newHole);
-    }
-    else
-    {
-      currentPlayer = PlayerOne;
-      Console.WriteLine("PlayerOne's turn, select a seeded hole from the bottom row");
-      newHoleInput = Console.ReadLine();
-      int.TryParse(newHoleInput, out newHole);
-    }
-		Game.PlayGame(currentPlayer, newHole);
+		{
+			CurrentPlayer = PlayerTwo;
+			Console.WriteLine("PlayerTwo's turn, select a seeded hole from the top row");		
+		}
+		else
+		{
+			CurrentPlayer = PlayerOne;
+			Console.WriteLine("PlayerOne's turn, select a seeded hole from the bottom row"); 
+		}
+
+		string newHoleInput = Console.ReadLine()!;
+		bool _ = int.TryParse(newHoleInput, out newHole);
+		PlayGame(CurrentPlayer, newHole);
 	}
 }
 
 public static class GameState
 {
-	public static void CheckGame(Player currentPlayer, Player PlayerOne, Player PlayerTwo)
+	public static void CheckGame()
 	{
 		for(int i = 0; i < Board.Holes.Length; i++)
 		{
 			if(Board.Holes[i] == 4)
 			{
 				if(i >= 0 && i < 6)
-					PlayerTwo.Score++;
+					Game.PlayerTwo.Score++;
 					
 				if(i >= 5 && i < 12)
-					PlayerOne.Score++;
+					Game.PlayerOne.Score++;
 			} 
-			else 
-			{
-				ReprintBoardWithStats(currentPlayer);
-			}
 		}
-	}
-	
-	public static void ReprintBoardWithStats(Player currentPlayer)
+        ReprintBoardWithStats();
+    }
+
+    public static void ReprintBoardWithStats()
 	{
-		Console.WriteLine($"Current Player {0}", currentPlayer.Index);
-		Console.WriteLine($"SeedCount {0}", Game.NumberOfPickedSeeds);
-	}
+        Console.WriteLine($"Current Player {Game.CurrentPlayer.Index}");
+        Console.WriteLine($"SeedCount {Game.NumberOfPickedSeeds}");
+    }
 }
 
 public class AI
